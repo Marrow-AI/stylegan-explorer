a00f72b8f62e15302f55177544a0217f0905182a
shirinstar <shirinanlen@gmail.com>
2021-08-25 17:44:09 -0700

    mobile restrict

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -6,6 +6,7 @@ import Home from './components/Home.js';
 import About from './components/About.js';
 import socketIOClient from "socket.io-client";
 import store, { setSocket } from './state'
+import { isMobile } from 'react-device-detect';
 
 const socket = socketIOClient('');
 
@@ -24,8 +25,32 @@ function App() {
     ENDPOINT: ENDPOINT
 })
 
+if (isMobile) {
+  return (
+    <div className='mobile-container'>
+      <div className="mobileContainer">
+        <p className="mobileMsg">This website is not suitable for mobile devices.
+          <br />
+          Please come back from your desktop.
+          <br />
+          <br />
+          Thank you! </p>
+      </div>
+    </div>
+  )
+}
   return (
     <>
+    <div className='mobile-container Main'>
+        <div className="mobileContainer">
+          <p className="mobileMsg">This website is not suitable for small size screens.
+            <br />
+            Please increase your window or revist from desktop.
+            <br />
+            <br />
+            Thank you! </p>
+        </div>
+      </div>
     <div className='app-container'>
       <Router>
         <Route exact path="/" component={Home} />
